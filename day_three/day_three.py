aacb8dbe1c403eb95d943f74b820832170109a59
Auréliane Dor <aurelianedor@auremacbook.lan>
2023-12-11 22:16:11 +0100

    wip day_three

--- day_three/day_three.py
+++ day_three/day_three.py
@@ -1,30 +1,61 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Grid:
-    x: str
-    y: str
-    x_index: int
-    y_index: int
+    CHARACTERS = "%$@&#/-+*="
 
-    def is_digit(self):
-        print(self.character.is_digit())
+    map: [str]
 
-@dataclass
-class Text:
-    path: str
-    lines: list
-    text_content: str = ''
+    def __init__(self, file: str):
+        with open(file) as f:
+            self.map = f.readlines()
 
-    def read_file(self):
-        with open(self.path) as file:
-            for line in file:
-                self.lines.append(line)
+    def get_character(self, y, x):
+        try:
+            return self.map[y][x]
+        except IndexError:
+            return '.'
+
+    def is_digit(self, y: int, x: int):
+        return self.get_character(y, x).isdigit()
+
+    def get_characters_around(self, y: int, x: int) -> [str]:
+        directions = [
+            (-1, -1), (-1, 0),  (-1, 1),
+            (0, -1),            (0, 1),
+            (1, -1), (1, 0),    (1, 1),
+        ]
+        characters = []
+        for (d_y, d_x) in directions:
+            characters.append(self.get_character(y + d_y, x + d_x))
+        return characters
+
+    def has_special_character(self, characters: [str]) -> bool:
+        for character in characters:
+            if character in Grid.CHARACTERS:
+                return True
+        return False
+
+    def get_number(self, y, x):
+        index = x
+        number = []
+        while self.is_digit(y, index):
+            number.append(self.get_character(y, index))
+            index = index + 1
+        return int(''.join(number))
+
 
 def main():
-    text = Text('text.txt', [])
-    text.read_file()
-    print(text.lines[0])
+    grid = Grid('text.txt')
+
+    for y, line in enumerate(grid.map):
+        for x, character in enumerate(line):
+            numbers_list = []
+            if grid.is_digit(y, x):
+                pass
+
+
 
 if __name__ == '__main__':
     main()
