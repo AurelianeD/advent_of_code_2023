60ffd437cdf92a9f12ea374e5022c08626b0f33d
Auréliane Dor <aureliane.dor@gmail.com>
2023-12-21 19:27:56 +0100

    wip

--- day_three/day_three.py
+++ day_three/day_three.py
@@ -1,6 +1,20 @@
 from dataclasses import dataclass
 
 
+def has_special_character(characters: [str]) -> bool:
+    for character in characters:
+        if character in Grid.CHARACTERS:
+            return True
+    return False
+
+
+@dataclass
+class Character:
+    line_index: int
+    character_index: int
+    character: str
+
+
 @dataclass
 class Grid:
     CHARACTERS = "%$@&#/-+*="
@@ -11,33 +25,27 @@ class Grid:
         with open(file) as f:
             self.map = f.readlines()
 
-    def get_character(self, y, x):
+    def get_character(self, y: int, x: int) -> str:
         try:
             return self.map[y][x]
         except IndexError:
             return '.'
 
-    def is_digit(self, y: int, x: int):
+    def is_digit(self, y: int, x: int) -> bool:
         return self.get_character(y, x).isdigit()
 
     def get_characters_around(self, y: int, x: int) -> [str]:
         directions = [
-            (-1, -1), (-1, 0),  (-1, 1),
-            (0, -1),            (0, 1),
-            (1, -1), (1, 0),    (1, 1),
+            (-1, -1), (-1, 0), (-1, 1),
+            (0, -1), (0, 1),
+            (1, -1), (1, 0), (1, 1),
         ]
         characters = []
         for (d_y, d_x) in directions:
             characters.append(self.get_character(y + d_y, x + d_x))
         return characters
 
-    def has_special_character(self, characters: [str]) -> bool:
-        for character in characters:
-            if character in Grid.CHARACTERS:
-                return True
-        return False
-
-    def get_number(self, y, x):
+    def get_number(self, y: int, x: int) -> int:
         index = x
         number = []
         while self.is_digit(y, index):
@@ -45,16 +53,31 @@ class Grid:
             index = index + 1
         return int(''.join(number))
 
+    def get_numbers_with_position(self, y: int, x: int) -> [Character]:
+        index = x
+        number = []
+        while self.is_digit(y, index):
+            character = self.get_character(y, index)
+            number.append(Character(y, index, character))
+            index = index + 1
+        return number
+
 
 def main():
     grid = Grid('text.txt')
 
-    for y, line in enumerate(grid.map):
-        for x, character in enumerate(line):
-            numbers_list = []
-            if grid.is_digit(y, x):
-                pass
-
+    numbers_list = []
+    for index_y, line in enumerate(grid.map):
+        for index_x, character in enumerate(line):
+            if grid.is_digit(index_y, index_x) and not grid.is_digit(index_y, index_x - 1):
+                number = [grid.get_number(index_y, index_x)]
+                number_length = len(str(number))
+                for int, index in enumerate(number):
+                    character_around = grid.get_characters_around(index_y, index_x + int)
+                    if has_special_character(character_around):
+                        numbers_list.append(number[0])
+    numbers_sum = sum(numbers_list)
+    print(numbers_sum)
 
 
 if __name__ == '__main__':
