4acb00ecce4c5af015542062783cc9237968e1a1
Auréliane Dor <aureliane.dor@gmail.com>
2023-12-22 11:27:15 +0100

    end of day three part two

--- day_three/day_three.py
+++ day_three/day_three.py
@@ -7,9 +7,16 @@ def has_special_character(characters: [str]) -> bool:
             return True
     return False
 
+
+
 @dataclass
 class Grid:
     CHARACTERS = "%$@&#/-+*="
+    DIRECTIONS = [
+        (-1, -1), (-1, 0), (-1, 1),
+        (0, -1), (0, 1),
+        (1, -1), (1, 0), (1, 1),
+    ]
 
     map: [str]
 
@@ -28,16 +35,26 @@ class Grid:
     def is_digit(self, y: int, x: int) -> bool:
         return self.get_character(y, x).isdigit()
 
+    def is_start(self, y: int, x: int) -> bool:
+        return self.get_character(y, x) == '*'
+
     def get_characters_around(self, y: int, x: int) -> [str]:
-        directions = [
-            (-1, -1), (-1, 0), (-1, 1),
-            (0, -1),          (0, 1),
-            (1, -1), (1, 0), (1, 1),
-        ]
+
         characters = []
-        for (d_y, d_x) in directions:
+        for (d_y, d_x) in self.DIRECTIONS:
             characters.append(self.get_character(y + d_y, x + d_x))
         return characters
+
+    def get_numbers_around(self, y: int, x: int) -> [int]:
+        numbers = []
+        for (d_y, d_x) in self.DIRECTIONS:
+            is_digit = self.is_digit(d_y + y, d_x + x)
+            if is_digit:
+                number_first_index = self.get_first_character_number_index(d_y + y, d_x + x)
+                number = self.get_number(d_y + y, number_first_index)
+                if number not in numbers:
+                    numbers.append(number)
+        return numbers
 
     def get_number(self, y: int, x: int) -> int:
         index = x
@@ -47,23 +64,44 @@ class Grid:
             index = index + 1
         return int(''.join(number))
 
+    def get_first_character_number_index(self, y: int, x: int) -> int:
+        index = x
+        while self.is_digit(y, index):
+            index = index - 1
+        return index + 1
+
 
 def main():
     grid = Grid('text.txt')
+    # grid = Grid('subject.txt')
 
-    numbers_list = []
+    numbers_list_one = []
+    numbers_list_two = []
+
+    def part_one(y: int, x: int):
+        if grid.is_digit(y, x) and not grid.is_digit(y, x - 1):
+            number = grid.get_number(y, x)
+            number_length = len(str(number))
+            for i, index in enumerate(range(number_length)):
+                character_around = grid.get_characters_around(y, x + i)
+                if has_special_character(character_around):
+                    numbers_list_one.append(number)
+                    break
+
+    def part_two(y: int, x: int):
+        if grid.is_start(y, x):
+            numbers_around = grid.get_numbers_around(y,x)
+            if len(numbers_around) == 2:
+                numbers_list_two.append(numbers_around[0] * numbers_around[1])
+
     for index_y, line in enumerate(grid.map):
         for index_x, _ in enumerate(line):
-            if grid.is_digit(index_y, index_x) and not grid.is_digit(index_y, index_x - 1):
-                number = grid.get_number(index_y, index_x)
-                number_length = len(str(number))
-                for i, index in enumerate(range(number_length)):
-                    character_around = grid.get_characters_around(index_y, index_x + i)
-                    if has_special_character(character_around):
-                        numbers_list.append(number)
-                        break
-    numbers_sum = sum(numbers_list)
-    print(numbers_sum)
+            part_one(index_y, index_x)
+            part_two(index_y, index_x)
+    part_one_response = sum(numbers_list_one)
+    part_two_response = sum(numbers_list_two)
+    print('Part one response:', part_one_response)
+    print('Part two response:', part_two_response)
 
 
 if __name__ == '__main__':
