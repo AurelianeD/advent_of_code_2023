3f476d338b306fe9c4f34695f1e2f4a08ef7dfe3
Auréliane Dor <aureliane.dor@gmail.com>
2023-12-22 17:01:07 +0100

    done day six part one

--- day_six/day_six.py
+++ day_six/day_six.py
@@ -11,13 +11,19 @@ def get_value(line: str) -> [int]:
     return numbers
 
 
-def calculate_distance(speed: int)
+def calculate_distance(time: int, max_time: int) -> int:
+    return time * (max_time - time)
 
 
 def numbers_of_different_ways(milliseconds: int, max_distance: int) -> [int]:
-    numbers = []
+    numbers = 0
     for millisecond in range(0, milliseconds + 1):
         time_holding_button = millisecond
+        distance = calculate_distance(time_holding_button, milliseconds)
+        if distance > max_distance:
+            numbers = numbers + 1
+    return numbers
+
 
 @dataclass
 class Game:
@@ -27,14 +33,21 @@ class Game:
         with open(file) as f:
             self.map = f.readlines()
 
+
 def main():
     game = Game('text.txt')
+    # game = Game('subject.txt')
     times = get_value(game.map[0])
     distances = get_value(game.map[1])
-    print(numbers_of_different_ways(times[0], distances[0]))
-    print(distances)
 
-    pass
+    round_one = numbers_of_different_ways(times[0], distances[0])
+    round_two = numbers_of_different_ways(times[1], distances[1])
+    round_three = numbers_of_different_ways(times[2], distances[2])
+    round_four = numbers_of_different_ways(times[3], distances[3])
+
+    result = round_one * round_two * round_three * round_four
+
+    print(result)
 
 
 if __name__ == '__main__':
